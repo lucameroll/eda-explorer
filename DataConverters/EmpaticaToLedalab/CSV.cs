9d807a4bbc32dfbc4b75b4f4c94f8c30014d2248
Jacob Thorn <jakethorn1.1@gmail.com>
2020-06-25 08:51:50 +0200

    empatica2ledalab project added

---
+++ DataConverters/EmpaticaToLedalab/CSV.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LNE.IO
+{
+	/// <summary>
+	/// Represents a CSV file
+	/// </summary>
+	public class CSV
+	{
+		public string Separator { get; set; } = ";";
+
+		public string AsString
+		{
+			get
+			{
+				var retval = "";
+				for (int i = 0; i <= height; i++)
+				{
+					for (int j = 0; j <= width; j++)
+					{
+						var hasValue = cells.TryGetValue((j, i), out var val);
+						retval += hasValue && val != null ? val.ToString() : "";
+						retval += Separator;
+					}
+
+					retval += "\n";
+				}
+
+				return retval;
+			}
+		}
+
+		private Dictionary<(int, int), string> cells = new Dictionary<(int, int), string>();
+		private int width = 0;
+		private int height = 0;
+
+		public CSV() : this(";")
+		{
+
+		}
+
+		public CSV(string separator)
+		{
+			Separator = separator;
+		}
+
+		public bool LoadFromFile(string path)
+		{
+			if (File.Exists(path))
+			{
+				LoadFromString(File.ReadAllText(path));
+				return true;
+			}
+			else
+			{
+				Console.Error.WriteLine($"The path ({path}) does not exist.");
+				return false;
+			}
+		}
+
+		public void LoadFromString(string csv)
+		{
+			var rows = csv.Split("\n");
+			foreach (var row in rows)
+			{
+				AppendRow(row.Split(Separator));
+			}
+		}
+
+		public void AppendRow(params object[] row)
+		{
+			SetRow(height, row);
+		}
+
+		public void AppendColumn(params object[] col)
+		{
+			SetColumn(width, col);
+		}
+
+		public void SetRow(int y, params object[] row)
+		{
+			for (int i = 0; i < row.Length; i++)
+			{
+				SetCell(i, y, row[i]);
+			}
+
+			width = Math.Max(width, row.Length);
+			height = Math.Max(height, y + 1);
+		}
+
+		public void SetColumn(int x, params object[] col)
+		{
+			for (int i = 0; i < col.Length; i++)
+			{
+				SetCell(x, i, col[i]);
+			}
+
+			width = Math.Max(width, x + 1);
+			height = Math.Max(height, col.Length);
+		}
+
+		public void SetCell(int x, int y, object val)
+		{
+			cells.Add((x, y), val.ToString());
+		}
+
+		public bool TryGetRow<T>(int y, out T[] result)
+		{
+			result = default;
+
+			if (y >= height)
+			{
+				Console.Error.WriteLine($"No row at position ({y})");
+				return false;
+			}
+
+			var row = new List<T>();
+			for (int i = 0; i < width; i++)
+			{
+				var success = TryGetCell(i, y, out T cell);
+				if (success)
+				{
+					row.Add(cell);
+				}
+			}
+
+			result = row.ToArray();
+			return true;
+		}
+
+		public bool TryGetColumn<T>(int x, out T[] result)
+		{
+			result = default;
+
+			if (x >= width)
+			{
+				Console.Error.WriteLine($"No column at position ({x})");
+				return false;
+			}
+
+			var col = new List<T>();
+			for (int i = 0; i < height; i++)
+			{
+				var success = TryGetCell(x, i, out T cell);
+				if (success)
+				{
+					col.Add(cell);
+				}
+			}
+
+			result = col.ToArray();
+			return true;
+		}
+
+		public bool TryGetCell<T>(int x, int y, out T result)
+		{
+			result = default;
+
+			// check cell
+			var success = cells.TryGetValue((x, y), out string cell);
+			if (success == false)
+			{
+				Console.Error.WriteLine($"No cell at position ({x}, {y}).");
+				return false;
+			}
+
+			// convert to T
+			object value = default;
+			if (typeof(T) == typeof(float))
+			{
+				success = float.TryParse(cell, out float f);
+				value = f;
+			}
+			else if (typeof(T) == typeof(int))
+			{
+				success = int.TryParse(cell, out int i);
+				value = i;
+			}
+			else if (typeof(T) == typeof(bool))
+			{
+				success = bool.TryParse(cell, out bool b);
+				value = b;
+			}
+			else if (typeof(T) == typeof(string))
+			{
+				success = true;
+				value = cell;
+			}
+
+			// output
+			if (success)
+			{
+				result = (T)value;
+				return true;
+			}
+			else
+			{
+				Console.Error.WriteLine($"Cell ({x}, {y}) could not be parsed to a {typeof(T)}.");
+				return false;
+			}
+		}
+
+		public void SaveAs(string path)
+		{
+			File.WriteAllText(path, AsString);
+		}
+	}
+}
---
+++ DataConverters/EmpaticaToLedalab/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using LNE.IO;
+
+namespace Empatica2Ledalab
+{
+	class Program
+	{
+		private static int DELETE_TAG { get; } = -1;
+
+		static void Main(string[] args)
+		{
+			// load files
+			var edaCSV = LoadCSVFile("EDA file path:");
+			var tagCSV = LoadCSVFile("Tag file path:");
+
+			if (edaCSV == null || tagCSV == null)
+				return;
+
+			// load necessary data
+			var success = true;
+			success &= edaCSV.TryGetCell(0, 0, out float startTime);
+			success &= edaCSV.TryGetCell(0, 1, out float frequency);
+			success &= edaCSV.TryGetColumn(0, out float[] column);
+			success &= tagCSV.TryGetColumn(0, out float[] tags);
+
+			if (success == false)
+				return;
+
+			// convert tag times from utc to the start of recording
+			tags = ConvertTagTimes(tags, startTime);
+
+			// output columns (time, eda + tag)
+			var outTimes = new float[column.Length - 2];
+			var outEdas = new float[column.Length - 2];
+			var outTags = new float[column.Length - 2];
+
+			// convert from empatica -> ledalab
+			for (int i = 2; i < column.Length; i++)
+			{
+				var im2 = i - 2;
+
+				var time = CalcTime(im2, frequency);
+				outTimes[im2] = time;		// time
+				outEdas[im2] = column[i];	// eda
+
+				// tag
+				for (int j = 0; j < tags.Length; j++)
+				{
+					var halfSampleRate = 1 / (frequency / 2);
+					if (tags[j] >= time - halfSampleRate && tags[j] <= time + halfSampleRate)
+					{
+						outTags[im2] = j + 1;
+						tags[j] = DELETE_TAG;
+						break;
+					}
+					else
+					{
+						outTags[im2] = 0;
+					}
+				}
+			}
+
+			// save ledalab file
+			var ledalabCSV = new CSV();
+			ledalabCSV.Separator = "\t";
+			ledalabCSV.AppendColumn(outTimes.Cast<object>().ToArray());
+			ledalabCSV.AppendColumn(outEdas.Cast<object>().ToArray());
+			ledalabCSV.AppendColumn(outTags.Cast<object>().ToArray());
+
+			Console.WriteLine("Output file path:");
+			var outputPath = Console.ReadLine();
+			ledalabCSV.SaveAs(outputPath);
+		}
+
+		private static CSV LoadCSVFile(string prompt)
+		{
+			Console.WriteLine(prompt);
+			var path = Console.ReadLine();
+
+			var csv = new CSV();
+			csv.Separator = "\t";
+			var success = csv.LoadFromFile(path);
+
+			return success ? csv : null;
+		}
+
+		private static float[] ConvertTagTimes(float[] tags, float startTime)
+		{
+			for (int i = 0; i < tags.Length; i++)
+			{
+				tags[i] -= startTime;
+			}
+
+			return tags;
+		}
+
+		private static float CalcTime(int row, float frequency)
+		{
+			return row * (1 / frequency);
+		}
+	}
+}
